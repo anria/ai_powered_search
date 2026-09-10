2fbb0f06d10ab0bbc3110e794cd3608099ef0413
Anria <anria_lev@yahoo.com>
2026-09-10 04:21:17 -0700

    Add files via upload

---
+++ ai_search_project/2_docker_run.sh
@@ -0,0 +1,7 @@
+docker run -d --name django-container -v .:/app -p 8000:8000 django-image 
+
+# After this, use the docker desktop to stop/start the 
+# django-container
+# -d detached mode
+
+
--- ai_search_project/4_docker_clear_cache.sh
+++ ai_search_project/4_docker_clear_cache.sh
@@ -1,5 +1,28 @@
-docker stop django-container
-docker rm django container
-docker stop django-image
+docker stop django-main django-container django-image
+
+docker rm django-main
+docker rm django-container
 docker rm django-image
-docker system prune
+
+# STOP Sorl/zookeepers
+docker stop solr-1000 solr-main zookeeper-main zookeeper solr-1000_solr_data solr-1000_zookeeper_data solr-1000_zookeeper_datalog
+
+# REMOVE ALL solr logs
+docker exec -it solr-main bash -c "rm -f /var/solr/logs/*.log*"
+docker exec -it solr-1000 bash -c "rm -f /var/solr/logs/*.log*"
+
+docker rm solr-main zookeeper-main 
+docker rmi solr:10.0.0 zookeeper:3.9 solr-1000_solr_data solr-1000_zookeeper_data solr-1000_zookeeper_datalog
+
+docker image prune  -a -f
+docker volume prune -a -f
+docker buildx prune -a -f
+docker buildx history rm --all
+docker system prune -a -f
+
+
+
+
+# sudo sh -c 'truncate -s 0 $(docker inspect --format="{{.LogPath}}" <YOUR_SOLR_CONTAINER_NAME>)'
+# REMOVE ALL Docker logs - hell yea!
+# sudo sh -c 'truncate -s 0 /var/lib/docker/containers/*/*-json.log'
